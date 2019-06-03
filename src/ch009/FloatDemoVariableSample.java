cce46203d252e3ffbb8e6c0f8da8d9ea0a898df2
yinchu <javayinchu2013@163.com>
2019-06-03 17:23:46 +0800

    Add FloatDemoVariableSample

---
+++ src/ch009/FloatDemoVariableSample.java
@@ -0,0 +1,44 @@
+package ch009;
+
+/**
+ * 演示float和double类型的变量声明
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class FloatDemoVariableSample {
+
+	public static void main(String[] args) {
+		/**
+		 * 定义个float类型变量f1，并把1赋值给它
+		 */
+		float f1 = 1;
+
+		/**
+		 * 定义个float类型变量f2，并把1.0赋值给它
+		 */
+		float f2 = 1.0f;
+
+		/**
+		 * 定义个float类型变量f3，并把1.0赋值给它
+		 */
+		float f3 = 0.1F;
+
+		/**
+		 * 定义个double类型变量d1，并把1赋值给它
+		 */
+		double d1 = 1;
+
+		/**
+		 * 定义个double类型变量d2，并把1.0赋值给它
+		 */
+		double d2 = 1.0;
+
+		/**
+		 * 定义个double类型变量d3，并把0.1赋值给它
+		 */
+		double d3 = 0.1;
+
+	}
+
+}
