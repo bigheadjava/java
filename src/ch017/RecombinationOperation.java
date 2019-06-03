eb125f34f89bfcffa5ad4cd353caf564168fa081
yinchu <javayinchu2013@163.com>
2019-06-03 23:44:49 +0800

    演示符合操作符用法

---
+++ src/ch017/RecombinationOperation.java
@@ -0,0 +1,43 @@
+package ch017;
+
+/**
+ * 演示符合操作符用法
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class RecombinationOperation {
+
+	public static void main(String[] args) {
+		int a = 8, b = 2;
+
+		System.out.println(a + b);
+		System.out.println(a += b);
+		System.out.println(a);
+
+		a = 8;
+		b = 2;
+		System.out.println(a - b);
+		System.out.println(a -= b);
+		System.out.println(a);
+
+		a = 8;
+		b = 2;
+		System.out.println(a * b);
+		System.out.println(a *= b);
+		System.out.println(a);
+
+		a = 8;
+		b = 2;
+		System.out.println(a / b);
+		System.out.println(a /= b);
+		System.out.println(a);
+
+		a = 8;
+		b = 2;
+		System.out.println(a % b);
+		System.out.println(a %= b);
+		System.out.println(a);
+	}
+
+}
