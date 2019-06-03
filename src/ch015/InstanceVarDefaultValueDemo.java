7600c04795d3f9ac3860318de5c3b242e552c687
yinchu <javayinchu2013@163.com>
2019-06-03 21:16:05 +0800

    Show instance variable default value

---
+++ src/ch015/InstanceVarDefaultValueDemo.java
@@ -0,0 +1,34 @@
+package ch015;
+
+import ch014.StaticVar;
+
+/**
+ * 用于演示成员变量的默认值
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class InstanceVarDefaultValueDemo {
+
+	private int int_var;
+	private boolean boolean_var;
+	private float float_var;
+	private double double_var;
+	private char char_var;
+
+	private StaticVar static_var;
+	private String string_var;
+
+	public static void main(String[] args) {
+		InstanceVarDefaultValueDemo var = new InstanceVarDefaultValueDemo();
+
+		System.out.println(" int_var = " + var.int_var);
+		System.out.println(" boolean_var = " + var.boolean_var);
+		System.out.println(" float_var = " + var.float_var);
+		System.out.println(" double_var = " + var.double_var);
+		System.out.println(" char_var = " + var.char_var);
+		System.out.println(" static_var = " + var.static_var);
+		System.out.println(" string_var = " + var.string_var);
+	}
+
+}
