e737282166a59d3c8b682f982e4c911970f9d380
yinchu <javayinchu2013@163.com>
2019-06-12 22:04:06 +0800

    演示内部接口

---
+++ src/ch047/inter/Sample1.java
@@ -0,0 +1,30 @@
+package ch047.inter;
+
+/**
+ * 演示内部接口
+ * @author yinchu
+ *
+ */
+public class Sample1 {
+	
+	public interface MyInterface{ //内部接口
+		public int add(int i, int j);
+	}
+	
+	private MyInterface mi = new MyInterface() { //匿名内部类实现了接口MyInterface
+		@Override
+		public int add(int i, int j) {
+			return i + j;
+		}
+	};
+	
+	public class InnerClass implements MyInterface{
+
+		@Override
+		public int add(int i, int j) {
+			System.out.println("...成员内部类实现了MyInterface接口...");
+			return i + j;
+		}
+		
+	}
+}
