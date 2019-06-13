6b43860042a3459f5dbce33a279617a913a60556
yinchu <javayinchu2013@163.com>
2019-06-13 19:23:52 +0800

    演示自定义泛型类

--- src/ch048/generic_class/Demo1.java
+++ src/ch048/generic_class/Demo1.java
@@ -6,11 +6,11 @@ package ch048.generic_class;
  * @author 程序猿大头
  *
  */
-public class Demo {
+public class Demo1 {
 
 	public static void main(String[] args) {
-		MyGenericClass<String> gClass1 = new MyGenericClass<String>("Hello");
-		MyGenericClass<Integer> gClass2 = new MyGenericClass<Integer>(123);
+		MyGenericClass gClass1 = new MyGenericClass("Hello");
+		MyGenericClass gClass2 = new MyGenericClass(123);
 		System.out.println("gClass1 => T: " + gClass1.getKey());
 		System.out.println("gClass2 => T: " + gClass2.getKey());
 	}
