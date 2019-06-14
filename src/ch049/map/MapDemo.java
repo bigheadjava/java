b3997a80985a445e57478dc0c48b859d39723e56
yinchu <javayinchu2013@163.com>
2019-06-14 16:37:52 +0800

    演示Map用法

--- src/ch049/map/MapDemo.java
+++ src/ch049/map/MapDemo.java
@@ -2,6 +2,7 @@ package ch049.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 演示Map用法
@@ -12,12 +13,17 @@ public class MapDemo {
 
 	public static void main(String[] args) {
 		Map<Integer, String> map = new HashMap<Integer, String>();
-		map.put(1, "abc");
+		map.put(1, "hello");
 		map.put(2, "abc");
 		map.put(3, "java");
 		map.put(1, "abc");
 		System.out.println(map.size());
-		System.out.println(map.get(3));
+		System.out.println(map.get(1));
+		
+		Set<Integer> keySet = map.keySet();
+		for(Integer i : keySet) {
+			System.out.println("Key: " + i + "; Value: "+ map.get(i));
+		}
 	}
 
 }
