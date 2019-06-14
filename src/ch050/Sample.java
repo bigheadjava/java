900ecc8b584695ec51d37980c616f7ae46a6e669
yinchu <javayinchu2013@163.com>
2019-06-14 22:12:48 +0800

    演示线程执行

--- src/ch050/Sample.java
+++ src/ch050/Sample.java
@@ -11,16 +11,18 @@ public class Sample extends Thread {
 	@Override
 	public void run() {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("自定义线程: " + i);
+			System.out.println(currentThread().getName() + " : " + i); //获取当前线程
 		}
 	}
 
 	public static void main(String[] args) {
-		Sample s = new Sample();
-		s.start();
-		for (int i = 0; i < 100; i++) {
-			System.out.println("主线程: " + i);
-		}
+		Sample s1 = new Sample();
+		Sample s2 = new Sample();
+		Sample s3 = new Sample();
+		s1.start(); // 线程1
+		s2.start(); // 线程2
+		s3.run(); // 主线程
+
 	}
 
 }
