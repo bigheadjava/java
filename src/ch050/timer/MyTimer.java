7c9b774cac98b23a6357eff861d75bff638ddae0
yinchu <javayinchu2013@163.com>
2019-06-17 21:30:35 +0800

    演示定时器Timer用法

---
+++ src/ch050/timer/MyTimer.java
@@ -0,0 +1,31 @@
+package ch050.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 演示定时器Timer用法
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class MyTimer extends Thread {
+	private int i = 10;
+
+	@Override
+	public void run() {
+		Timer timer = new Timer(false);
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				if(i == 0) {
+					timer.cancel(); //倒计时到0为止，任务结束
+				}
+				System.out.println("正在执行倒计时: " + i--);
+			}
+
+		};
+		timer.schedule(task, 3000, 1000); //3000ms后开始执行任务，以后每间隔1000ms重复执行一次任务
+	}
+}
---
+++ src/ch050/timer/TimerDemo.java
@@ -0,0 +1,16 @@
+package ch050.timer;
+
+/**
+ * 演示Timer定时器用法
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class TimerDemo {
+
+	public static void main(String[] args) {
+		MyTimer mt = new MyTimer();
+		mt.start();
+	}
+
+}
