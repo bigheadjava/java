7576107e80d652b37cf01abdd4c2700da3fc9da9
yinchu <javayinchu2013@163.com>
2019-06-17 22:48:35 +0800

    模拟打破原子操作

--- src/ch050/sync/bank/v1/Demo.java
+++ src/ch050/sync/bank/v1/Demo.java
@@ -10,41 +10,39 @@ package ch050.sync.bank.v1;
  */
 public class Demo {
 
+	public class BankSystem extends Thread {
+		private Account acc;
+
+		public BankSystem() {
+
+		}
+
+		public BankSystem(Account acc) {
+			this.acc = acc;
+		}
+
+		@Override
+		public void run() {
+			double amount = acc.getAmount();
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			acc.setAmount(amount - 1000);
+		}
+	}
+
 	public static void main(String[] args) throws InterruptedException {
 		Account acc = new Account("BANK001", "Ma Jun", 10000);
-		Thread wife = new Thread() {
-			@Override
-			public void run() {
-				double amount = acc.getAmount();
-				try {
-					sleep(500);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				acc.setAmount(amount - 1000);
-				System.out.println(currentThread().getName() + " : " + acc.toString());
-			}
-		};
-
-		Thread husband = new Thread() {
-			@Override
-			public void run() {
-				double amount = acc.getAmount();
-				try {
-					sleep(500);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				acc.setAmount(amount - 1000);
-				System.out.println(currentThread().getName() + " : " + acc.toString());
-			}
-		};
+		Demo.BankSystem wife = new Demo().new BankSystem(acc);
+		Demo.BankSystem husband = new Demo().new BankSystem(acc);
 		wife.setName("老婆取钱");
 		husband.setName("老公取钱");
 		wife.start();
 		husband.start();
 		Thread.sleep(1000);
-		System.out.println(acc.getAmount());
+		System.out.println(acc.toString());
 	}
 
 }
