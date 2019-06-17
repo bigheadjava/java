8476eb306eacf90f777d88d1a6e0dced63afd0a5
yinchu <javayinchu2013@163.com>
2019-06-17 16:50:44 +0800

    演示软引用

--- src/ch045/reference/soft/SoftReferenceDemo.java
+++ src/ch045/reference/soft/SoftReferenceDemo.java
@@ -17,7 +17,7 @@ public class SoftReferenceDemo {
 		String str = new String("Java");
 		SoftReference<String> softReference = new SoftReference<>(str, referenceQueue);
 		str = null; //
-		System.gc(); // 通知JVM来进行垃圾回收，回收刚刚的new ReferenceQueue<>()
+		System.gc(); // 通知JVM来进行垃圾回收，回收刚刚的String对象
 		
 		System.out.println(softReference.get()); // Java，假如String对象被回收，softReference.get()返回null
 
