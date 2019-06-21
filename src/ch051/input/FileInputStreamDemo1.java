2e59a696d6599a563c66169c9722a4ea69df368d
yinchu <javayinchu2013@163.com>
2019-06-21 20:04:21 +0800

    演示FileInputStream用法

---
+++ src/ch051/input/FileInputStreamDemo1.java
@@ -0,0 +1,22 @@
+package ch051.input;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * 演示FileInputStream用法
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class FileInputStreamDemo1 {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("/Users/resource/codes/java/src/ch051/files/file1.txt");
+		int data;
+		while((data = fis.read()) != -1) {
+			System.out.print(data + " ");
+		}
+	}
+
+}
